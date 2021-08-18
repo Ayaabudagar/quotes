7ae23a42db8fca972834bffade62fe7dc186514e
Ayaabudagar <ayakhlil23@yahoo.com>
2021-08-18 23:03:30 +0300

    done

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -3,59 +3,45 @@
  */
 package quotes;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
 public class App {
+
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        try{
-            URL url = new URL("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
-            HttpURLConnection connect = (HttpURLConnection) url.openConnection();
-            connect.setRequestMethod("GET");
-            connect.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-            int status = connect.getResponseCode();
-            if(status == 200){
-                InputStream input = connect.getInputStream();
-                InputStreamReader inputRead = new InputStreamReader(input);
-                BufferedReader bufRead = new BufferedReader(inputRead);
-                String line = bufRead.readLine();
 
-                System.out.println(line);
+        try {
+            FileReader jsonFile = new FileReader("recentquotes.json");
 
-                bufRead.close();
-            } else{
-                System.out.println("locally");
-                FileReader jsonFile = new FileReader("recentquotes.json");
+            ArrayList<Quotes> quotes= readJson(jsonFile);
+            int r = (int) (Math.random() * (quotes.size()));
+            System.out.println(quotes.get(r).toString());
 
-                ArrayList<Quotes> q= jsonFileReading(jsonFile);
-                int rand = (int) (Math.random() * (q.size()));
-                System.out.println(q.get(rand).toString());
-            }
-
-            connect.disconnect();
-
-
-
-
-
-        } catch (IOException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
-    public static ArrayList<Quotes> jsonFileReading(FileReader jsonFile){
+
+
+
+
+    public  static ArrayList<Quotes> readJson(FileReader jsonFile) {
         Gson gson = new Gson();
         BufferedReader reader = new BufferedReader(jsonFile);
-        ArrayList<Quotes> quotes =gson.fromJson(reader,new TypeToken<ArrayList<Quotes>>() {}.getType());
-        try{
+        ArrayList<Quotes> quotes = gson.fromJson(reader,new TypeToken<ArrayList<Quotes>>() {}.getType());
+        try {
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -63,4 +49,3 @@ public class App {
         return quotes;
     }
 }
-
